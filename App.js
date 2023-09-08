2a603b894bf4a1d4ebeab7aebfaa1dd138f1e714
PhanTien234 <minhtienp328@gmail.com>
2023-09-08 11:39:03 +0700

    Communicate by POST method

--- App.js
+++ App.js
@@ -1,4 +1,3 @@
-// App.js
 import React, { useState } from "react";
 import {
   View,
@@ -24,10 +23,29 @@ const App = () => {
     }
   };
 
+  const handlePostRequest = async () => {
+    try {
+      const response = await axios.post(
+        "https://jsonplaceholder.typicode.com/posts",
+        {
+          title: "New Item",
+          body: "This is a new item created using a POST request.",
+          userId: 1,
+        }
+      );
+      setData([...data, response.data]);
+    } catch (error) {
+      console.error("Error creating item:", error);
+    }
+  };
+
   return (
     <View style={styles.container}>
       <TouchableOpacity onPress={handleGetRequest} style={styles.button}>
         <Text style={styles.buttonText}>Fetch Data</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={handlePostRequest} style={styles.button}>
+        <Text style={styles.buttonText}>Create New Item</Text>
       </TouchableOpacity>
       <FlatList
         data={data}
@@ -38,24 +56,4 @@ const App = () => {
   );
 };
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#fff",
-  },
-  button: {
-    backgroundColor: "blue",
-    padding: 10,
-    borderRadius: 5,
-    marginVertical: 10,
-  },
-  buttonText: {
-    color: "#fff",
-    fontSize: 16,
-    fontWeight: "bold",
-  },
-});
-
-export default App;
+// ...styles remain the same
